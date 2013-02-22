d343a11571595bb207dd433c39155620ca8487d3
Willem-Jan Zijderveld <willemjan@beeldspraak.com>
2013-02-22 12:07:45 +0100

    Initial commit

---
+++ MigrateDateProperties.php
@@ -0,0 +1,105 @@
+<?php
+use Symfony\Component\Console\Command\Command;
+use Jackalope\Session;
+use Jackalope\Node;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Migrate Date fields with format('r') to format('Y-m-d H:i:s')
+ *
+ * @author Willem-Jan Zijderveld <wjzijderveld@gmail.com>
+ */
+class MigrateDateProperties extends Command
+{
+    /** @var bool */
+    protected $dryRun;
+
+    /** @var \Symfony\Component\Console\Output\OutputInterface */
+    protected $output;
+
+    /** @var array */
+    protected $updates = array();
+
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('jackalope:dbal:migrate-date-properties')
+            ->setDescription('Migrates dates to the new format')
+            ->setDefinition(array(
+                new InputOption('dry-run', 't', InputOption::VALUE_NONE,
+                    'Instead of updateing, show the paths that would be updated'
+                )
+            ))
+            ->setHelp(<<<EOT
+Converts all Date properties in format 'r' to format 'Y-m-d H:i:s'
+EOT
+            );
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->dryRun = $input->getOption('dry-run');
+        $this->output = $output;
+
+        /** @var $session \Jackalope\Session */
+        $session = $this->getHelper('phpcr')->getSession();
+
+        $this->fixNode($session->getRootNode());
+
+        if (!$this->dryRun) {
+            $session->save();
+            $totalProperties = 0;
+
+            foreach ($this->updates as $update) {
+                $totalProperties += count($update);
+            }
+
+            $output->writeln(sprintf('<info>Updated %d nodes with %d Date properties</info>', count($this->updates), $totalProperties));
+        } else {
+            if (count($this->updates)) {
+                $output->writeln('Would have updated the following nodes and properties');
+                foreach ($this->updates as $path => $properties) {
+                    $output->writeln('<info>' . $path . '</info>');
+                    foreach ($properties as $property) {
+                        $output->writeln("\t" . $property);
+                    }
+                }
+            } else {
+                $output->writeln('No nodes found with Date properties');
+            }
+        }
+    }
+
+    protected function fixNode(Node $node)
+    {
+        $this->fixNodeProperties($node);
+
+        foreach ($node as $child) {
+            $this->fixNode($child);
+        }
+    }
+
+    protected function fixNodeProperties(Node $node)
+    {
+        $properties = $node->getProperties();
+
+        /** @var $property \Jackalope\Property */
+        foreach ($properties as $property) {
+            if ($property->getType() === \PHPCR\PropertyType::DATE) {
+                if (!$this->dryRun) {
+                    // Just mark property as modified
+                    // The data retrieved from DB is correct, it just needs to be saved again in the new format
+                    $property->setModified();
+                }
+
+                $this->updates[$node->getPath()][] = $property->getName() . sprintf(' (<comment>%s</comment> => <comment>%s</comment>)', $property->getValue()->format('r'), $property->getValue()->format('Y-m-d H:i:s'));
+            }
+        }
+    }
+
+}
---
+++ bin/migrate.php
@@ -0,0 +1,56 @@
+#!/usr/bin/env php
+<?php
+
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
+}
+
+$configFile = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'cli-config.php';
+if (!file_exists($configFile) || !is_readable($configFile)) {
+    die('You must create a cli-config.php in the root file to be able to run this command. See cli-config.php.dist for an example'.PHP_EOL);
+}
+
+require $configFile;
+
+if (!isset($dbConfig) || !is_array($dbConfig)) {
+    die('You should define a $dbConfig variable in cli-config.php'.PHP_EOL);
+}
+
+$missingKeys = array();
+foreach (array('hostname', 'username', 'password', 'database') as $key) {
+    if (!isset($dbConfig[$key])) {
+        $missingKeys[] = $key;
+    }
+}
+
+if (count($missingKeys)) {
+    die('You are missing the following keys in $dbConfig: '. join(', ', $missingKeys) . PHP_EOL);
+}
+
+$connection = \Doctrine\DBAL\DriverManager::getConnection(array(
+    'driver'    => isset($dbConfig['driver']) ? $dbConfig['driver'] : 'pdo_mysql',
+    'host'      => $dbConfig['hostname'],
+    'user'      => $dbConfig['username'],
+    'password'  => $dbConfig['password'],
+    'dbname'    => $dbConfig['database'],
+));
+
+$phpcr_user = isset($phpcr['user']) ? $phpcr['user'] : $dbConfig['username'];
+$phpcr_pass = isset($phpcr['pass']) ? $phpcr['pass'] : $dbConfig['password'];
+$workspace  = isset($phpcr['workspace']) ? $phpcr['workspace'] : 'default';
+
+$repository = Jackalope\RepositoryFactoryDoctrineDBAL::getRepository(array('jackalope.doctrine_dbal_connection' => $connection));
+$session = $repository->login(new \PHPCR\SimpleCredentials($phpcr_user, $phpcr_pass), $workspace);
+
+$cli = new \Symfony\Component\Console\Application('Jackalope Doctrine DBAL Migration');
+$cli->setCatchExceptions(true);
+$cli->setHelperSet(new \Symfony\Component\Console\Helper\HelperSet(array(
+    'session'   => new \PHPCR\Util\Console\Helper\PhpcrHelper($session),
+)));
+$cli->addCommands(array(
+    new MigrateDateProperties(),
+));
+
+$cli->run();
